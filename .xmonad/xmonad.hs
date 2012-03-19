862cdf7136d54bcc196f5b42a925b19a89823d36
Jochen Maes <github@gcool.be>
2012-03-19 12:26:01 +0100

    code cleanup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,11 +8,6 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig  
 import XMonad.Layout.NoBorders(smartBorders)  
 import XMonad.Layout.PerWorkspace  
-import XMonad.Layout.Spacing
-import XMonad.Layout.IM  
-import XMonad.Layout.Grid  
-import XMonad.Actions.GridSelect  
-import Data.Ratio ((%))  
 import XMonad.Actions.CycleWS  
 import qualified XMonad.StackSet as W  
 import System.IO  
@@ -52,11 +47,12 @@ main = do
 
 -- Open dmenu
         [ ((mod4Mask, xK_r), spawn "exe=`dmenu_run -b -nb black -nf blue -sf white` && eval \"exec $exe\"")
+
 -- Open programs
           , ((mod4Mask, xK_Return), spawn "urxvt")
           , ((mod4Mask, xK_f), spawn "firefox")
           , ((mod1Mask, xK_F4), kill)
-          , ((mod4Mask, xK_m), spawn "virtualbox")
+          , ((mod4Mask, xK_m), spawn "urxvt -e mc")
           , ((mod4Mask, xK_t), spawn "truecrypt")
           , ((mod4Mask, xK_w), spawn "wireshark")
           , ((mod4Mask, xK_c), spawn "celestia")
@@ -65,9 +61,13 @@ main = do
           , ((mod4Mask, xK_h), spawn "handbrake")
           , ((mod4Mask, xK_v), spawn "vlc")
           , ((mod4Mask, xK_g), spawn "gimp")
+
 -- Shutdown/reboot
           , ((mod4Mask .|. mod1Mask, xK_F4), spawn "urxvt -e sudo shutdown -h now")
           , ((mod4Mask .|. mod1Mask, xK_r), spawn "urxvt -e sudo reboot")
+
+-- Hetzner ssh
+          , ((mod4Mask, xK_s), spawn "/home/gcool/Hetzner/clean.sh")
  
 -- Lock screen
           , ((mod4Mask .|. mod1Mask, xK_l), spawn "xscreensaver-command --lock")
